b71aecca74ada568aa6cda5a440741c9ad601209
Mikulas Plesak <mikulas.plesak@gmail.com>
2020-03-05 15:24:24 +0530

    settings icon and dropdown created

--- roboarm01/jquery.js
+++ roboarm01/jquery.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
-    // toggle the add task overlay
-    $('.add-task-toggle').click(function () {
-        if ($('.add-task-toggle-text').text() == 'ADD') {
-            $('.add-task-toggle-text').text('BACK');
-            $('.add-task-overlay').css({
+    // toggle the settings dropdown
+    $('.settings_button').click(function () {
+        console.log("FUNCTION CALLED!");
+        if ($('.settings_dropdown').css("visibility") == "hidden") {
+            console.log("SHOWING");
+            $('.settings_dropdown').css({
                 visibility: 'visible',
                 opacity: 0
             }).animate({
                 opacity: 1
             }, 300);
-            //$('.map').css('visibility', 'visible');
-        } else { // switch from MAP to GAME
-            $('.add-task-toggle-text').text('ADD');
-            $('.add-task-overlay').css({
+        } else {
+            console.log("HIDING!");
+            $('.settings_dropdown').css({
                 opacity: 1
             }).animate({
                 opacity: 0
             }, 300, function () {
-                $('.add-task-overlay').css('visibility', 'hidden');
+                $('.settings_dropdown').css({
+                visibility: 'hidden'});
             });
-            //$('.map').css('visibility', 'hidden');
         }
     });
 });
