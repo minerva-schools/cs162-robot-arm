bf36e0eaa2ff3c63ff68d811e353dba5814cb9fd
GeraValdez <gera@minerva.kgi.edu>
2020-04-23 17:54:07 -0700

    This push should hopefully create a new branch with much of the backend. It adds new functionalities such as automatically redirecting a user to the main room, limiting one person in the main room at all times, and limiting your time in the main room to five minutes. Moreover, there is a control room that looks over all the users and can be used to keep track of the users or for testing purposes

--- web/app.py
+++ web/app.py
@@ -1,18 +1,26 @@
 #Importing All the needed libraries
-from . import db, app, login
+from __init__ import db, app, login
 import os #To handle files path
 from flask import Flask, render_template, redirect, request, g, session #Main Flask
 from flask_login import LoginManager, UserMixin, current_user, login_user, login_required, logout_user #To create the Login
 from flask_sqlalchemy import SQLAlchemy #SQL Alchemy to create the database
 import sys
+from datetime import datetime
+import time
 from werkzeug.security import generate_password_hash, check_password_hash
-from .robotmodel.python_to_aduino import forward_kin_end, forward_kin_mid, coordinates_to_angles
+#from robotmodel.python_to_aduino import forward_kin_end, forward_kin_mid, coordinates_to_angles
+
 #Creating the primary database
 class User(UserMixin, db.Model): # A table to store users data
     __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(200))
     password = db.Column(db.String(200))
+    student = db.Column(db.Integer) #Equals 1 if this is a student, and 0 otherwise
+    logged_in = db.Column(db.Integer, default=0) #Equals 1 if logged in, and 0 if you are not logged in
+    in_cue = db.Column(db.Integer, default=0) #Equals 1 if user is waiting for access to the robot arm, and 0 if not
+    time = db.Column(db.DateTime, default=datetime.utcnow) #Records the time the user requests access to the robot
+                                                           #arm controls. Time is used for queueing purposes
     def __repr__(self):
         return "<Username: {}>".format(self.username)
 
@@ -28,6 +36,10 @@ This function redirects users to the Login page
 """
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
+    if current_user is not None:
+        current_user.logged_in = 0 #update the database
+        db.session.commit()
+        logout_user()      #If logged in, every time you go to the home page you will be automatically logged out
     return render_template('index.html')
 
 @app.route('/observe')
@@ -38,6 +50,29 @@ def observe():
 def help():
     return render_template("help.html")
 
+
+@app.route('/controlroom') #This is a master controlroom that will show
+@login_required              #information about the users.
+def controlroom():
+    list_of_all_users = User.query.all()
+    # Only the robot team and the professor should have access to the controlroom
+    # There is no button to redirect to the controlroom so the admin should know the url
+    # The user needs to be logged in as of the admin, or else will be redirected to observe
+    if current_user.username == 'gera@minerva.kgi.edu' or \
+                                'mikulas.plesak@minerva.kgi.edu' or \
+                                'ara.mkhoyan@minerva.kgi.edu' or \
+                                'quang.tran@minerva.kgi.edu' or \
+                                'a.kamel@minerva.kgi.edu' or \
+                                'arham.hameed@minerva.kgi.edu' or \
+                                'psterne@minerva.kgi.edu':
+        return render_template('controlroom.html', userlist=list_of_all_users)
+    else:
+        error_message = 'The control room is only for admin. Please log in'
+        return render_template('controlroom.html', message=error_message)
+
+@app.route('/timer')
+def timer():
+    return render_template('timer.html')
 
 @login.user_loader
 def load_user(id):
@@ -67,8 +102,14 @@ def register():
             error = 'Passwords do not match. Please, try again.'
             return render_template('index.html', error_register=error)
 
+        # check if user is a student
+        if "@minerva.kgi.edu" in username:
+            is_student = 1
+        else:
+            is_student = 0
+
         # store user information, with password hashed
-        new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
+        new_user = User(username=username, password=generate_password_hash(password, method='sha256'), student=is_student)
         db.session.add(new_user)
         db.session.commit()
         register_success_message = 'Registered successful. Please log in'
@@ -78,8 +119,9 @@ def register():
 
 
 """
-This function is responsible for logging users in to their personal kanban board
-This function validates users credentials, if the user is registered it redirects to the Kanban board
+This function is responsible for logging users to the server.
+This function validates user credentials.
+If the user is registered it redirects to the observe page of the robotic arm
 If the user is not registered it displays an error that the user is not registered
 """
 @app.route('/login', methods=['GET', 'POST'])
@@ -94,7 +136,9 @@ def login():
             error = 'The password or the username you entered is not correct!'
             return render_template('index.html', message=error)
         login_user(user)
-        return redirect('/main')
+        user.logged_in = 1 #update the database
+        db.session.commit()
+        return redirect('/waitroom')
         # return render_template('main.html')
     elif request.method == 'GET':
         return render_template('index.html')
@@ -105,12 +149,14 @@ This function is responsible for logging users out
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
+    user.logged_in = 0 #update the database
+    db.session.commit()
     logout_user()
     return redirect('login')
 
 """
-This is the primary function responsible for displaying the tasks
-This function does not allow duplicate tasks to exist
+This is the main room, where the user can observe the stream of the robot arm
+This function requires the user to be logged in using his credentials or as guest
 """
 @app.route('/main', methods=["GET", "POST"])
 @login_required
@@ -144,6 +190,46 @@ def process_coordinates():
                'theta1': str(round(theta1,2)), 'theta2': str(round(theta2,2)), 'theta3': str(round(theta3,2))}
     return render_template("main.html", sent_back_coordinates=True, **content)
 
+@app.route('/waitroom', methods=["GET", "POST"])
+@login_required
+def wait():    
+    '''
+    This function retrieves a list of all users waiting to use the robot arm, and sorts it
+    '''
+    print(current_user, file=sys.stderr)
+    print(current_user.id, file=sys.stderr)
+    user_list = User.query.filter_by(in_cue=0).all() #returns the list of users waiting to use the robot
+    user_list.sort(key=lambda User: User.time)                  #sorts the list by who came first
+    user_list.sort(key=lambda User: User.student, reverse=True) #sorts the list by who is a student
+    print(user_list, file=sys.stderr)
+    print(user_list[0].id, file=sys.stderr)
+
+    if current_user.id == user_list[0].id:
+        success_message = 'There is an available robot arm for you. You will be redirected shortly'
+        return render_template('waitroom.html', message=success_message)
+        #time.sleep(60)
+        #return redirect('/main')
+
+    else:
+        return render_template("waitroom.html", myuser=current_user)
+        ## Tell people how many people in front of them in the cue
+        ## Tell people how much time they should expect to wait
+        ## Create a timer for people in the control/main room and tell them you only have 5 minutes
+
+    #controller = User.query.filter_by(username=user_list[0]).first()
+    #print(controller, file=sys.stderr)
+    
+    #if current_user.id == user_list[0].id:
+    
+
 #Running the application
 if __name__ == "__main__":
     app.run(debug=True)
+
+# Cue System for the Robot Controls
+
+
+#print ('Hello world!', file=sys.stderr)
+
+#redirect user_list[0] i.e., the first person in the queue. He shall have five minutes owning the controls
+#redirect all other users to the waiting room, and display how many people are in front of them and how much time they should expect to wait
