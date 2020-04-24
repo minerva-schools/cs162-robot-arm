d2437cd427ae7fa7924a37a6eac342da92454c79
Ahmed Kamel <a.kamel@minerva.kgi.edu>
2020-04-24 16:13:54 -0300

    Edison to Arduino
    This file will be the file running on the Intel Edison. This file constantly pulls updates from the website (every 5 seconds) and send them to the Arduino for execution through the serial port.

---
+++ Edison_to_Arduino.py
@@ -0,0 +1,21 @@
+import serial
+import time
+import urllib.request
+# specify the port below (e.g., serial.Serial('com4')).
+# to know which port we are using, go to the arduino, click Tools
+#arduinoData = serial.Serial('com4')
+
+# url to download from
+URL = 'http://www.robotarm.net/download'
+t = 5 # delay time (in seconds)
+while True:
+    # download from url, save to command_info.txt
+    urllib.request.urlretrieve(URL,'command_info.txt')
+    # read line by line (the 3 angles) from the .txt
+    file_object = open(r"command_info.txt", "r")
+    for i, line in enumerate(file_object):
+        angle = str(line)
+        print(type(angle),angle)
+        #arduinoData.write(angle)
+
+    time.sleep(5)
